9b0b67bf98f2e56b1484653dac676712b97802b9
SaadHussein <saadwithyara@gmail.com>
2023-07-15 15:52:03 +0300

    Add Arrows to Slider

--- src/components/Cars/Cars.jsx
+++ src/components/Cars/Cars.jsx
@@ -5,6 +5,42 @@ import classes from "./Cars.module.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        borderRadius: "50%",
+        height: "18px",
+        width: "19px",
+        background: "brown",
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        borderRadius: "50%",
+        height: "18px",
+        width: "19px",
+        background: "brown",
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
 function Cars() {
   const settings = {
     dots: true,
@@ -12,6 +48,8 @@ function Cars() {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
   return (
     <div className={classes.cars}>

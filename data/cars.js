9de2e0e28d24d29128398e0a7a180ba1d74d01a8
SaadHussein <sa3dhusseein@gmail.com>
2023-07-15 23:27:25 +0300

    Complete Cars Section

--- data/cars.js
+++ data/cars.js
@@ -1,6 +1,9 @@
 import carFive from "../assets/car5.png";
 import carSix from "../assets/car6.png";
 import carSeven from "../assets/car7.png";
+import carOne from "../assets/car4.png";
+import carTwo from "../assets/car1.png";
+import carThree from "../assets/car3.png";
 
 export const cars = [
   {
@@ -10,7 +13,7 @@ export const cars = [
       "The Porsche 911 is a true icon in the sports car world, known for its sleek design, impressive performance, and superior handing. it's a two-door, two-seat coupe that's perfect for those who love to feel the wind in their hair and the road beneath their wheels. ",
     seats: 2,
     luggage: 2,
-    image: carSeven,
+    image: carTwo,
   },
   {
     name: "BMW 7 Series",
@@ -19,7 +22,7 @@ export const cars = [
       "The BMW 7 Series is the ultimate luxury car, offering unparalleled comfort, style, and technology. with its sleek exterior and spacious interior, this four-door sedan is perfect for those who demand the very best in automotive design and performance.",
     seats: 5,
     luggage: 3,
-    image: carSix,
+    image: carOne,
   },
   {
     name: "Range Rover Sport",
@@ -28,7 +31,7 @@ export const cars = [
       "The Range Rover Sport is a versatile SUV that's perfect for both on and off-road adventures. with its powerful engine and advanced four-wheel drive system, this vehicle can handle any terrain with ease. It's also incredibly spacious, with room for up to seven passengers and plenty of cargo space.:",
     seats: 7,
     luggage: 4,
-    image: carFive,
+    image: carThree,
   },
   {
     name: "Mercedes-Benz S-Class",
@@ -45,7 +48,7 @@ export const cars = [
       "The Tesla Model S is a high-performance electric car that's perfect for those who want to combine luxury with sustainability. with its sleek design and advanced technology,. this four-door sedan offers a smooth and quiet ide. It also has plenty of space for passengers ond luggage",
     seats: 5,
     luggage: 2,
-    image: carSix,
+    image: carFive,
   },
   {
     name: "Bentley Bentayga",
@@ -54,6 +57,6 @@ export const cars = [
       "The Bentley Bentayga is the ultimate luxury suw, offering unparalleled comfort, style, and performance. With its powerful engine and advanced technology, this vehicle can handle any terrain with ease. It's also incredibly spacious, with room for up to seven passengers and plenty of cargo space.",
     seats: 7,
     luggage: 4,
-    image: carFive,
+    image: carSix,
   },
 ];
